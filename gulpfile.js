53ca7e23527c311f7d2cd075e9470777a13db717
Anil Maharjan <simpleanilcha@gmail.com>
2015-09-04 16:20:53 +0545

    only leveluptuts course completed. keep on learning Gulp and other.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require ('gulp'),
 	uglify = require ('gulp-uglify'),
-	sass = require ('gulp-ruby-sass');
+	sass = require ('gulp-ruby-sass'),
+	plumber = require ('gulp-plumber')
+	livereload = require('gulp-livereload'),
+	imagemin = require('gulp-imagemin'),
+	prefix = require('gulp-autoprefixer');
+
+// function for errolog
+function errorLog(error){
+	console.error.bind(error);
+	this.emit('end');
+}
 
 // Scripts Task
 // Uglifies
 gulp.task('scripts', function(){
 	gulp.src('js/*.js')
+	//.pipe(plumber())
 	.pipe(uglify())
+	.on('error', errorLog)
 	.pipe(gulp.dest('minjs'));
 });
 
@@ -19,14 +31,30 @@ gulp.task('scripts', function(){
 // sass Task
 // styles
 gulp.task('styles', function(){
-	return sass('scss/', { style: 'compressed' })
+	return sass('scss/', { style: 'expanded' })
+	//.pipe(plumber())
+	//.on('error', console.error.bind(console))
+	.on('error', errorLog)
 	.on('error', sass.logError)
-	.pipe(gulp.dest('css'));
+	.pipe(prefix('last 2 versions'))
+	.pipe(gulp.dest('css'))
+	.pipe(livereload());
+	});
+
+// Image Task
+// Minify images
+gulp.task('image', function(){
+	gulp.src('images/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('build/img'));
 	});
 
 // Watch Task
 // Watches
 gulp.task('watch', function(){
+	//livereload function
+	//var server = livereload();
+	livereload.listen();
 	gulp.watch('js/*.js', ['scripts']);
 	gulp.watch('scss/**/*.scss', ['styles']);
 	})
